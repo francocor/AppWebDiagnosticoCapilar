f7c8f5d602c5dce6e701f6d7de22f6dcd9e9aef3
franco cornejo <francocornejoc15@gmail.com>
2024-09-22 00:37:29 -0300

    cambios en header

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -6,11 +6,7 @@ const Footer = () => {
     <div>
         <footer className="footer">
       <div className="footer-content">
-        <div className="footer-info">
-          <p>Dirección: Calle Ejemplo 123, Ciudad, País</p>
-          <p>Teléfono: +123 456 789</p>
-          <p>Email: contacto@ejemplo.com</p>
-        </div>
+        
         <div className="footer-copy">
           <img src={TecItali} alt="Logo" className="footer-logo" />
           <p>&copy; 2024 Tec Italy. Todos los derechos reservados.</p>
